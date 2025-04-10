840ece9ef16765ba6b5ae084f5eeada4c256ad1e
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-09 22:49:25 -0300

    Create 1004-Produto-Simples.java

---
+++ 1004-Produto-Simples.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner Scanner = new Scanner(System.in);
+
+        int num1 = Scanner.nextInt();
+        int num2 = Scanner.nextInt();
+
+        int prod = num1 * num2;
+
+        System.out.println("PROD = "+prod);
+
+    }
+}
