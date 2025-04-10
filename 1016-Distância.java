1b7f537f15958714491e8b78a425e8d49b2e550e
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-09 23:14:37 -0300

    Create 1016-Distância.java

---
+++ 1016-Distância.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner Scanner = new Scanner(System.in);
+
+        int km = Scanner.nextInt();
+
+        int minutos = km * 2;
+
+        System.out.println(minutos+" minutos");
+
+    }
+}
